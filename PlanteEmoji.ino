d7f886f436d814957538aa3f8970e3d4a76fbf36
Yassin Aghani <yassin@Yassins-MacBook-Pro.local>
2016-08-11 12:37:07 +0200

    Rename leds to matrice

--- PlanteEmoji.ino
+++ PlanteEmoji.ino
@@ -9,11 +9,11 @@
    There will only be a single MAX7221 attached to the arduino
 */
 
-LedControl leds = LedControl(12, 11, 10, 1);
+LedControl matrice = LedControl(12, 11, 10, 1);
 
 void setup() {
-  leds.clearDisplay(0);
-  leds.shutdown(0, false);
+  matrice.clearDisplay(0);
+  matrice.shutdown(0, false);
   Serial.begin(9600);
 }
 
@@ -22,67 +22,66 @@ void loop() {
   Serial.println(humidity);
 
   if(humidity > 300){
-    leds.clearDisplay(0);
+    matrice.clearDisplay(0);
     // ================ BAD ================
-    leds.setLed(0,1,1,true);
-    
-    leds.setLed(0,2,2,true);
-    leds.setLed(0,3,2,true);
-    leds.setLed(0,4,2,true);
-    leds.setLed(0,5,2,true);
-    
-    leds.setLed(0,6,1,true);
-  
-  
-    leds.setLed(0,1,5,true);
-    leds.setLed(0,2,5,true);
-    leds.setLed(0,2,6,true);
-  
-    leds.setLed(0,6,5,true);
-    leds.setLed(0,5,5,true);
-    leds.setLed(0,5,6,true);
+    matrice.setLed(0,1,1,true);
+
+    matrice.setLed(0,2,2,true);
+    matrice.setLed(0,3,2,true);
+    matrice.setLed(0,4,2,true);
+    matrice.setLed(0,5,2,true);
+
+    matrice.setLed(0,6,1,true);
+
+
+    matrice.setLed(0,1,5,true);
+    matrice.setLed(0,2,5,true);
+    matrice.setLed(0,2,6,true);
+
+    matrice.setLed(0,6,5,true);
+    matrice.setLed(0,5,5,true);
+    matrice.setLed(0,5,6,true);
   } else {
     // ================ SMILE ================
-    leds.clearDisplay(0);
-    leds.setLed(0,1,2,true);
-    
-    leds.setLed(0,2,1,true);
-    leds.setLed(0,3,1,true);
-    leds.setLed(0,4,1,true);
-    leds.setLed(0,5,1,true);
-    
-    leds.setLed(0,6,2,true);
-  
-  
-    leds.setLed(0,1,5,true);
-    leds.setLed(0,2,5,true);
-    leds.setLed(0,2,6,true);
-    leds.setLed(0,1,6,true);
-  
-    leds.setLed(0,6,5,true);
-    leds.setLed(0,6,6,true);
-    leds.setLed(0,5,5,true);
-    leds.setLed(0,5,6,true);
+    matrice.clearDisplay(0);
+    matrice.setLed(0,1,2,true);
+
+    matrice.setLed(0,2,1,true);
+    matrice.setLed(0,3,1,true);
+    matrice.setLed(0,4,1,true);
+    matrice.setLed(0,5,1,true);
+
+    matrice.setLed(0,6,2,true);
+
+
+    matrice.setLed(0,1,5,true);
+    matrice.setLed(0,2,5,true);
+    matrice.setLed(0,2,6,true);
+    matrice.setLed(0,1,6,true);
+
+    matrice.setLed(0,6,5,true);
+    matrice.setLed(0,6,6,true);
+    matrice.setLed(0,5,5,true);
+    matrice.setLed(0,5,6,true);
   }
 
- 
+
   /*
-  leds.setLed(0,1,2,true);
-  
-  leds.setLed(0,2,1,true);
-  leds.setLed(0,3,1,true);
-  leds.setLed(0,4,1,true);
-  leds.setLed(0,5,1,true);
-  
-  leds.setLed(0,6,2,true);
+  matrice.setLed(0,1,2,true);
 
-  leds.setLed(0,2,4,true);
-  leds.setLed(0,2,5,true);
-  leds.setLed(0,2,6,true);
+  matrice.setLed(0,2,1,true);
+  matrice.setLed(0,3,1,true);
+  matrice.setLed(0,4,1,true);
+  matrice.setLed(0,5,1,true);
 
-  leds.setLed(0,5,4,true);
-  leds.setLed(0,5,5,true);
-  leds.setLed(0,5,6,true);
+  matrice.setLed(0,6,2,true);
+
+  matrice.setLed(0,2,4,true);
+  matrice.setLed(0,2,5,true);
+  matrice.setLed(0,2,6,true);
+
+  matrice.setLed(0,5,4,true);
+  matrice.setLed(0,5,5,true);
+  matrice.setLed(0,5,6,true);
   */
 }
-
